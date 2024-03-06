9b5dd2be8aa34488aefa5c70fede15339d6c6f7f
sh4869 <nobuk4869@gmail.com>
2024-03-07 00:26:05 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,27 @@ type Done = {
 };
 
 const App = () => {
-  const [tasks, setTasks] = useState<{ [key: string]: any }[]>([
+  const [tasks, setTasks] = useState<Done[]>([
     { key: "one", dones: [dayjs()] },
-    { key: "two" },
+    { key: "two", dones: [] },
   ]);
 
   return (
     <>
       <div>
         {tasks.map((v, i) => {
-          return <p key={i}>{v["key"]}</p>;
+          return (
+            <div key={i} className="">
+              <h2 className="inline-block text-2xl mr-4">{v.key}</h2>
+              {v.dones.map((date) => {
+                return (
+                  <p className="inline-block text-base">
+                    {date.format("YYYY/MM/DD")}
+                  </p>
+                );
+              })}
+            </div>
+          );
         })}
       </div>
     </>
