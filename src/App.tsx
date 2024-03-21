58c6004de823142049b8864e44c6fd975e1789c2
sh4869 <nobuk4869@gmail.com>
2024-03-22 00:08:26 +0900

    最低限

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,55 @@ type Done = {
   dones: dayjs.Dayjs[];
 };
 
-const Dones = ({ done }: { done: Done }) => {
+const Dones = ({
+  done,
+  onChange,
+}: {
+  done: Done;
+  onChange: (done: Done) => void;
+}) => {
+  const days = new Array(7).fill(0).map((_, i) => dayjs().add(-i, "day"));
+  const dates = days.map((v) => {
+    const has = done.dones.filter((z) => z.isSame(v, "day"));
+    if (has.length > 0) {
+      return <span>{" * "}</span>;
+    } else {
+      return <span>{" _ "}</span>;
+    }
+  });
+
+  const addDate = () => {
+    onChange({
+      ...done,
+      dones: [...done.dones, dayjs()],
+    });
+  };
+
   return (
     <div>
-      <h2 className="inline-block text-2xl mr-4">{done.key}</h2>
-      {new Array(7).fill(0)}
+      <h2 className="inline-block text-xl mr-4">{done.key}</h2>
+      <div className="inline-block">{dates}</div>
+      <div className="inline-block">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 mx-2 rounded"
+          onClick={addDate}
+        >
+          add
+        </button>
+      </div>
     </div>
   );
 };
 
 const App = () => {
   const [tasks, setTasks] = useState<Done[]>([
-    { key: "one", dones: [dayjs()] },
-    { key: "two", dones: [] },
+    { key: "家の掃除", dones: [dayjs().add(-1, "day")] },
+    { key: "洗面の流し掃除", dones: [] },
   ]);
+
+  const onChange = (done: Done, i: number) => {
+    setTasks([...tasks.slice(0, i), done, ...tasks.slice(i + 1)]);
+  };
 
   return (
     <>
@@ -27,14 +62,7 @@ const App = () => {
         {tasks.map((v, i) => {
           return (
             <div key={i} className="">
-              <h2 className="inline-block text-2xl mr-4">{v.key}</h2>
-              {v.dones.map((date) => {
-                return (
-                  <p className="inline-block text-base">
-                    {date.format("YYYY/MM/DD")}
-                  </p>
-                );
-              })}
+              <Dones done={v} onChange={(v) => onChange(v, i)} />
             </div>
           );
         })}
